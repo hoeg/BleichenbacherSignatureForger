24c2233fd9c752c6079623adf14bacc6bab26261
hoeg <peter.steffensen@gmail.com>
2017-06-29 21:11:17 +0000

    added arg parsing (WIP)

--- Forge.py
+++ Forge.py
@@ -8,7 +8,17 @@ from SignatureForger import *
 if __name__ == "__main__":
   parser = argparse.ArgumentParser(description="Signature forger for RSA PKCS1v1.5 given that the exponent 3 is used and the verification algorithm is not implemented properly")
 
-  keySize = 0
-  hashAlg = None
+  parser.add_argument("-k", "--keysize", type=int)
+  parser.add_argument("-ha", "--hashalg", action="store", type=str, choices=list(HASH_ASN1.keys()))
+  parser.add_argument("-m", "--message", type=str)
+
+  args = parser.parse_args()
+
+  keySize = args.keysize
+  hashAlg = args.hashalg
 
   signatureForger = SignatureForger(keySize, hashAlg)
+
+  signature = signatureForger.forgeSignature_method2(args.message)
+  print(hex(toInt(signature) ** 3))
+
--- SignatureForger.py
+++ SignatureForger.py
@@ -28,69 +28,69 @@ def toBytes(val):
 
 class SignatureForger:
 
-	def __init__(self, keysize, hashAlg):
-		self.keysize = keysize
-		self.hashAlg = hashAlg
+  def __init__(self, keysize, hashAlg):
+    self.keysize = keysize
+    self.hashAlg = Hash(hashAlg)
 
-	def encodePkcs1Suffix(self, message):
-		messageHash = self.hashAlg.digester(message).digest()
-		if ord(messageHash[-1]) & 0x01 != 0x01:
-			print("Hash value must be uneven. Try a different message")
-			exit()
-		suffix = "\x00" + self.hashAlg.digestInfo + messageHash
-		return suffix
-	
-	def constructSignatureSuffix(self, suffix):
-		signatureSuffix = 1
-		for idx in range(len(suffix) * 8):
-			if getBitAt(idx, signatureSuffix ** 3) != getBitAt(idx, toInt(suffix)):
-				signatureSuffix = setBitAt(idx, signatureSuffix)
-		return toBytes(signatureSuffix)
+  def encodePkcs1Suffix(self, message):
+    messageHash = self.hashAlg.digester(message).digest()
+    if ord(messageHash[-1]) & 0x01 != 0x01:
+      print("Hash value must be uneven. Try a different message")
+      exit()
+    suffix = "\x00" + self.hashAlg.digestInfo + messageHash
+    return suffix
+  
+  def constructSignatureSuffix(self, suffix):
+    signatureSuffix = 1
+    for idx in range(len(suffix) * 8):
+      if getBitAt(idx, signatureSuffix ** 3) != getBitAt(idx, toInt(suffix)):
+        signatureSuffix = setBitAt(idx, signatureSuffix)
+    return toBytes(signatureSuffix)
 
-	def addPrefixToSignature(self, signatureSuffix):
-		prefix = "\x00\x01"
-		prefix += "\xFF"*8
-		while True:
-			testPrefix = prefix + os.urandom((self.keysize/8) - (len(prefix)))
-			signatureCandidate = toBytes(int(gmpy2.cbrt(toInt(testPrefix))))[:-len(signatureSuffix)] + signatureSuffix
-			toCheck = toBytes(toInt(signatureCandidate) ** 3)
-			if "\x00" not in toCheck[:-len(signatureSuffix)]:
-				return signatureCandidate
+  def addPrefixToSignature(self, signatureSuffix):
+    prefix = "\x00\x01"
+    prefix += "\xFF"*8
+    while True:
+      testPrefix = prefix + os.urandom((self.keysize/8) - (len(prefix)))
+      signatureCandidate = toBytes(int(gmpy2.cbrt(toInt(testPrefix))))[:-len(signatureSuffix)] + signatureSuffix
+      toCheck = toBytes(toInt(signatureCandidate) ** 3)
+      if "\x00" not in toCheck[:-len(signatureSuffix)]:
+        return signatureCandidate
 
 
-	def forgeSignature_method1(self, message):
-		suffix = self.encodePkcs1Suffix(message)
-		signatureSuffix = self.constructSignatureSuffix(suffix)
-		signature = self.addPrefixToSignature(signatureSuffix)
-		return signature
+  def forgeSignature_method1(self, message):
+    suffix = self.encodePkcs1Suffix(message)
+    signatureSuffix = self.constructSignatureSuffix(suffix)
+    signature = self.addPrefixToSignature(signatureSuffix)
+    return signature
 
-	def nthroot (self, n, A, precision=300):
-	    getcontext().prec = precision
-	 
-	    n = Decimal(n)
-	    x_0 = A / n #step 1: make a while guess.
-	    x_1 = 1     #need it to exist before step 2
-	    while True:
-		#step 2:
-		x_0, x_1 = x_1, (1 / n)*((n - 1)*x_0 + (A / (x_0 ** (n - 1))))
-		if x_0 == x_1:
-		    return x_1
+  def nthroot (self, n, A, precision=300):
+    getcontext().prec = precision
+   
+    n = Decimal(n)
+    x_0 = A / n #step 1: make a while guess.
+    x_1 = 1     #need it to exist before step 2
+    while True:
+      #step 2:
+      x_0, x_1 = x_1, (1 / n)*((n - 1)*x_0 + (A / (x_0 ** (n - 1))))
+      if x_0 == x_1:
+        return x_1
 
-	def forgeSignature_method2(self, message, psLength=8):
-		prefix = "\x00\x01"
-		prefix += "\xFF"*psLength
-		suffix = self.encodePkcs1Suffix(message)
-		plain = prefix + suffix + "\x00"*((self.keysize/8)-(len(prefix) + len(suffix)))
+  def forgeSignature_method2(self, message, psLength=8):
+    prefix = "\x00\x01"
+    prefix += "\xFF"*psLength
+    suffix = self.encodePkcs1Suffix(message)
+    plain = prefix + suffix + "\x00"*((self.keysize/8)-(len(prefix) + len(suffix)))
     signature = toBytes(int(self.nthroot(3, toInt(plain)))+1)
-		return signature
+    return signature
 
 if __name__ == "__main__":
   message = "WhAAASDAatWhatInTheButt"
-  signatureForger = SignatureForger(1024, Hash("SHA-1"), 1)
+  signatureForger = SignatureForger(1024, "SHA-1", 1)
   #signature = signatureForger.forgeSignature_method1(message)
   signature = signatureForger.forgeSignature_method2(message)
   print(hex(toInt(signature) ** 3))
-	
+  
 '''
 output format: raw, hex, base64
 keysize: raw, from public key
