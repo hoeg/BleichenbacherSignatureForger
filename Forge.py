55a53460743167293bfc8123660e16b4aa955fff
hoeg <peter.steffensen@gmail.com>
2017-07-02 19:36:31 +0000

    added some cheesy output printer fucntion

--- Forge.py
+++ Forge.py
@@ -5,20 +5,45 @@ import argparse
 from HashInfo import *
 from SignatureForger import *
 
-if __name__ == "__main__":
+def printOutput(signature):
+  print("****************************************************")
+  print("*         Signature successfully generated!        *")
+  print("****************************************************")
+  print("")
+  print("Signature:")
+  print(hex(signature))
+  print("Plaintext:")
+  print(hex(toInt(signature) ** 3))
+
+
+def getArguments():
   parser = argparse.ArgumentParser(description="Signature forger for RSA PKCS1v1.5 given that the exponent 3 is used and the verification algorithm is not implemented properly")
 
   parser.add_argument("-k", "--keysize", type=int)
   parser.add_argument("-ha", "--hashalg", action="store", type=str, choices=list(HASH_ASN1.keys()))
+  parser.add_argument("-va", "--variant", type=int, choices=[1,2])
+  parser.add_argument("-of", "--outputformat", type=str, choices=["hex", "base64", "raw"])
   parser.add_argument("-m", "--message", type=str)
 
   args = parser.parse_args()
+  return args
+
+if __name__ == "__main__":
+
+  args = getArguments()
 
   keySize = args.keysize
   hashAlg = args.hashalg
 
   signatureForger = SignatureForger(keySize, hashAlg)
 
-  signature = signatureForger.forgeSignature_method2(args.message)
-  print(hex(toInt(signature) ** 3))
+  signature = ""
 
+  if args.variant == 1:
+    signature = signatureForger.forgeSignature_method1(args.message)
+  elif args.variant == 2:
+    signature = signatureForger.forgeSignature_method2(args.message)
+  else:
+    raise Exception("Unsupported signature method") 
+
+  printOutput(signature)
